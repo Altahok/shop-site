586adcfa970a69d2cc3bc6f198b55ea4b4c4ffaa
Altahok <altahok4@yandex.ru>
2022-01-15 22:43:07 +0400

    master

---
+++ script.js
@@ -0,0 +1,7 @@
+function print_contact() {
+   alert('skype: helper \n telegram: @helper \n tel: 89649956652')
+}
+
+function ThePresent() {
+   prompt('АКЦИЯ!! Введите имя друга рассказавшего вам о нашем магазине и вы с ним ПОЛУЧИТЕ СКИДКУ в 25%')
+}
